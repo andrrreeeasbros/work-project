d39298905a13bc7bcd34893b4c55f81d98ab5b97
Matvey Kamosko <ukik4974@gmail.com>
2024-11-14 23:16:31 +0300

    Add MainWindow and setting btn

---
+++ UnityApp/Assets/Scripts/WinMain/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace WinMain
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
